aa13f5ef78118a2c1493e8199cbecc6efe8c3916
paulinecoudert <67103663+paulinecoudert@users.noreply.github.com>
2023-06-20 16:52:15 +0200

    update link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,41 +9,43 @@ import Ui from "../assets/flexiAir.jpg";
 
 const data = [
   {
-    title: "IDECO",
+    title: "FLEXI AIR",
     img: Ui,
-    language: "PHP , Symphony, JS, Bootstrap ",
+    language: "FIGMA  ",
     lien: "https://www.figma.com/file/rAI0vtkW9p8UsPhGgETBJW?embed_host=share&kind=&node-id=26578%3A3219&t=3VGM2s4ZD96mONLm-1&type=design&viewer=1 ",
   },
   {
     title: "IDECO",
     img: IDeco,
     language: "PHP , Symphony, JS, Bootstrap ",
-    lien: ["https://www.delftstack.com/fr/howto/react/react-get-current-url/"],
-    email: "paulinecoudert@gmail.com",
+    lien: ["https://github.com/paulinecoudert/SymfonyDeco"],
   },
   {
-    title: "IDECO",
+    title: "UI TELECOM",
     img: UiTelecom,
-    language: "PHP , Symphony, JS, Bootstrap ",
-    lien: "https://www.delftstack.com/fr/howto/react/react-get-current-url/",
+    language: "FIGMA, HTML, CSS",
+    lien: "https://paulinecoudert.github.io/uiTelecom/",
   },
   {
-    title: "Mine Shop",
+    title: "MINE SHOP",
     img: MineShop,
     language: "Vue.JS , Bootstrap, TypeScript, HTML",
-    lien: "",
+    lien: "https://github.com/paulinecoudert/mine-store",
   },
   {
-    title: "Music Creator",
+    title: "MUSIC CREATOR",
     img: DaftPunkImg,
     language: "React, Javascript, CSS, HTML ",
-    lien: "",
+    lien: "https://paulinecoudert.github.io/daft_music/",
   },
 ];
 
 const Projects = () => {
   return (
-    <div id="projects" className="max-w-[1040px] m-auto sm:pr-8 md:pl-20 p-0 pb-16">
+    <div
+      id="projects"
+      className="max-w-[1040px] m-auto sm:pr-8 md:pl-20 p-0 pb-16"
+    >
       <h1 className=" border-t-2 border-dotted border-gray-400  left-13  animate-pulse mx-8 text-8xl font-bold text-left  text-indigo-500">
         Pro
       </h1>
